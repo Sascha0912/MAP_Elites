e02ead5cc2a6eecc879519fb7fa8de4dbc625f36
Sascha Obst <info@saschaobst.de>
2019-02-21 17:11:52 +0100

    fixed rastrigin fitness function

--- domain/rastrigin/rastrigin_FitnessFunc.py
+++ domain/rastrigin/rastrigin_FitnessFunc.py
@@ -3,12 +3,19 @@ import math
 import pandas as pd
 def rastrigin_FitnessFunc(pop):
     # TODO: hier muss noch angepasst werden auf 2 inputs
+    print("pop")
+    print(pop)
     def rastr(x):
-        return (20 + (x**2 - 10.0 * np.cos(2 * math.pi * x)))/40
+        summ = 0
+        summ += x[0]**2 - 10.0 * np.cos(2 * math.pi * x[0])
+        summ += x[1]**2 - 10.0 * np.cos(2 * math.pi * x[1])
+        return (20 + summ)/40
+         
     genes = []
     for i in range(len(pop)):
         genes.append(pop[i])
     df = pd.DataFrame(data=genes)
+    print(df)
     df_fitness = pd.DataFrame(data=rastr(df))
     df_fitness = df_fitness.transpose()
 
